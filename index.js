562d7a8c4e4489a03b0be0bce207b0c56a8d25f8
Belsicc <143416443+Belsicc@users.noreply.github.com>
2024-03-01 20:52:47 +0100

    index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-  res.send("Hello! Welcome back!")
+  res.send("Proton Support")
 })
 
 app.listen(3000, () => {
-  console.log("Project is ready!")
+  console.log("Proton Support")
 })
 
 let Discord = require("discord.js")
@@ -14,7 +14,7 @@ let client = new Discord.Client()
 
 
 client.on('ready', () => {
- client.user.setActivity(`1.0.1 your prefix here`, { type: 'PLAYING'})
+ client.user.setActivity(`Proton Support`, { type: 'PLAYING'})
 })
 
 client.login(process.env.token);
